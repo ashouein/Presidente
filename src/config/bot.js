51579b1451e9baaff4dd802275ea888c52cff83f
ashouein <kateashleynavarez@gmail.com>
2026-06-06 12:22:46 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Supremo",
+        name: "Selfie - RealestK",
         // Activity type number (0 = Playing).
         type: 2, 
       },
